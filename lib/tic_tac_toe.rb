f8836c14df60ee95a94939f26d8252320f89fc2a
S. Jones <jonesitis7@gmail.com>
2018-08-14 14:01:34 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,6 +37,19 @@ def valid_move?(board, index)
   end
 end
 
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.chomp
+  index = input_to_index(index)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    puts "Please enter 1-9:"
+    turn(board)
+end
+end
+
 def turn_count(board)
 turns = 0
 board.each do |space| # space is a local variable to hold each position in the board array
@@ -56,15 +69,50 @@ else
 end
 end
 
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.chomp
-  index = input_to_index(index)
-  if valid_move?(board, index)
-    move(board, index, current_player(board))
-    display_board(board)
+def won?(board)
+  WIN_COMBINATIONS.detect do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1] # load the value of the board at win_index_1
+    position_2 = board[win_index_2] # load the value of the board at win_index_2
+    position_3 = board[win_index_3] # load the value of the board at win_index_3
+
+      if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return win_combination
+      elsif !board.all?
+        false
+      else
+        false
+	   end
+  end
+end
+
+def full?(board)
+  board.all? {|token| token == "X" || token == "O"}
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    true
   else
-    puts "Please enter 1-9:"
-    turn(board)
+    false
+  end
+end
+
+def over?(board)
+if won?(board) || draw?(board) || full?(board)
+  true
+else
+  false
+end
+end
+
+def winner(board)
+win_combination = won?(board)
+if win_combination
+  win_location = win_combination[0]
+  board[win_location]
 end
 end
