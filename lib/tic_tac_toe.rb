05d48d6163b96c1ccedb5ea129559de56435c0bc
S. Jones <jonesitis7@gmail.com>
2018-08-14 13:59:52 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -61,7 +61,7 @@ def turn(board)
   input = gets.chomp
   index = input_to_index(index)
   if valid_move?(board, index)
-    move(board, index, current_player)
+    move(board, index, current_player(board))
     display_board(board)
   else
     puts "Please enter 1-9:"
